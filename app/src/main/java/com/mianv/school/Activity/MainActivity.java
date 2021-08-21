97f0bb295d66cf5f1756833a8ee6545c39955fbb
vahag <vahagn.mian@gmail.com>
2021-08-21 11:02:20 +0400

    Finished icon creation

--- app/src/main/java/com/mianv/school/Activity/MainActivity.java
+++ app/src/main/java/com/mianv/school/Activity/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     DashboardAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
     ArrayList<Card> cards;
-    ScrollView scrollView;
+     ScrollView scrollView;
 
 
 
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         viewInitialization();
         createRecyclerView();
         addTestItemsToRecyclerView();
-        //Overscrolling animation
+       // Overscrolling animation
         OverScrollDecoratorHelper.setUpOverScroll(scrollView);
 
 
@@ -69,18 +69,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addTestItemsToRecyclerView(){
-        Card card1 = new Card(R.drawable.section_arrows_2,"Թեմա 1" , "Մանևրում, դասավորվածություն \nnերթևեկաելի \nnմասում..." , 13, 61);
+        Card card1 = new Card(R.drawable.ic_arrows,"Թեմա 1" , "Մանևրում, դասավորվածություն \nերթևեկաելի \nմասում..." , 13, 61);
+        Card card2 = new Card(R.drawable.ic_law_hummer,"Թեմա 2" , "Օրենք" , 13, 61);
+        Card card3 = new Card(R.drawable.ic_gears,"Թեմա 3" , "Շահագործում" , 13, 61);
+        Card card4 = new Card(R.drawable.ic_road_sign,"Թեմա 4" , "Նշաններ" , 13, 61);
+        Card card5 = new Card(R.drawable.ic_cross_road_1,"Թեմա 5" , "Խաչմերուկ 1" , 13, 61);
+        Card card6 = new Card(R.drawable.ic_cross_road_2,"Թեմա 6" , "Խաչմերուկ 2" , 13, 61);
+        Card card7 = new Card(R.drawable.ic_parking,"Թեմա 7" , "Ճանապարհային գծանշում, կանգառ, կայանում" , 13, 61);
+        Card card8 = new Card(R.drawable.ic_big_car,"Թեմա 8" , "Արագություն, քարշակում, մարդկանց և բեռնեռրի փոխադրում" , 13, 61);
+        Card card9 = new Card(R.drawable.ic_waning_polygon,"Թեմա 9" , "Նախազգուշացնող ազդանշաններ, հատուկ ազդանշաններ․․․" , 13, 61);
+        Card card10 = new Card(R.drawable.ic_medkit,"Թեմա 10" , "Երթևեկության մասնակիցների կյանքի անվտանգություն․․․" , 13, 61);
+
+
 
         cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
-        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+        cards.add(card4);
+        cards.add(card5);
+        cards.add(card6);
+        cards.add(card7);
+        cards.add(card8);
+        cards.add(card9);
+        cards.add(card10);
+
+
     }
 
     public void viewInitialization(){
