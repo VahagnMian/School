5383d584cec0c32b44db0c8e26163bb4205b4f29
vahag <vahagn.mian@gmail.com>
2021-08-25 02:01:28 +0400

    First Section Questions ready

--- app/src/main/java/com/mianv/school/Activity/QuizActivity.java
+++ app/src/main/java/com/mianv/school/Activity/QuizActivity.java
@@ -1,12 +1,24 @@
 package com.mianv.school.Activity;
 
+import static com.mianv.school.Util.Constants.getAllQuestions;
+import static com.mianv.school.Util.Constants.question002;
+import static com.mianv.school.Util.Constants.questions;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.mianv.school.Model.Question;
 import com.mianv.school.R;
+import com.mianv.school.Util.Constants;
+
+import java.util.ArrayList;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -17,19 +29,125 @@ public class QuizActivity extends AppCompatActivity {
     RadioButton radioButton4;
     RadioButton radioButton5;
 
+    Button nextQuestion;
+    Button previousQuestion;
+
+    TextView questionText;
+    ImageView questionImage;
+    ArrayList<Question> questions;
+    int currentIndex = 0;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        viewInitialization();
 
+        if(currentIndex == 0){
+            previousQuestion.setVisibility(View.GONE);
+        }
+
+
+
+        bindQuestionToViews(questions.get(currentIndex));
+
+        nextQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex++;
+                if(currentIndex == questions.size()-1){
+                    nextQuestion.setVisibility(View.GONE);
+                }
+
+
+                if (currentIndex>0){
+                    previousQuestion.setVisibility(View.VISIBLE);
+                }
+
+
+                Question currentQuestion = questions.get(currentIndex);
+                bindQuestionToViews(currentQuestion);
+            }
+        });
+        previousQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                currentIndex--;
+
+                if (currentIndex != questions.size()){
+                    nextQuestion.setVisibility(View.VISIBLE);
+                }
+
+                    Question currentQuestion = questions.get(currentIndex);
+                    bindQuestionToViews(currentQuestion);
+
+                    if (currentIndex == 0){
+                        previousQuestion.setVisibility(View.GONE);
+                    }
+
+                }
+        });
+
+
+    }
+
+    public void bindQuestionToViews(Question question){
+        questionText.setText(question.getQuestionText());
+        if (question.getImageResID() != 0){
+                 questionImage.setImageResource(question.getImageResID());
+        }else {
+            questionImage.setVisibility(View.GONE);
+        }
+
+        radioButton1.setText(question.getOptionOne());
+        radioButton2.setText(question.getOptionTwo());
+
+
+        if (question.getOptionThree() == ""){
+            radioButton3.setVisibility(View.GONE);
+        }else {
+            radioButton3.setText(question.getOptionThree());
+        }
+
+        if (question.getOptionFour() == ""){
+            radioButton4.setVisibility(View.GONE);
+        }else {
+            radioButton4.setText(question.getOptionFour());
+        }
+
+        if (question.getOptionFive() == ""){
+            radioButton5.setVisibility(View.GONE);
+        }else {
+            radioButton5.setText(question.getOptionFive());
+
+        }
+
+
+
+
+    }
+
+    public void viewInitialization(){
+        questionText = findViewById(R.id.questionText);
+        questionImage = findViewById(R.id.questionImage);
         radioGroup = findViewById(R.id.radioGroup);
         radioButton1 = findViewById(R.id.radioButtonVariant1);
         radioButton2 = findViewById(R.id.radioButtonVariant2);
         radioButton3 = findViewById(R.id.radioButtonVariant3);
         radioButton4 = findViewById(R.id.radioButtonVariant4);
         radioButton5 = findViewById(R.id.radioButtonVariant5);
-
-
-
+        nextQuestion = findViewById(R.id.nextQuestionButton);
+        previousQuestion = findViewById(R.id.previousQuestionButton);
+        questions = getAllQuestions();
     }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/mianv/school/Model/Question.java
@@ -0,0 +1,169 @@
+package com.mianv.school.Model;
+
+public class Question {
+
+    int id;
+    int imageResID;
+    int correctAnswer;
+    String questionText;
+    String optionOne= "";
+    String OptionTwo= "";
+    String OptionThree= "";
+    String OptionFour= "";
+    String OptionFive="";
+
+    public Question(int id, int imageResID, int correctAnswer, String questionText, String optonOne, String optionTwo, String optionThree, String optionFour, String optionFive) {
+
+        this.id = id;
+        this.imageResID = imageResID;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optonOne;
+        OptionTwo = optionTwo;
+        OptionThree = optionThree;
+        OptionFour = optionFour;
+        OptionFive = optionFive;
+    }
+
+    public Question(int id, int correctAnswer, String questionText, String optonOne, String optionTwo, String optionThree, String optionFour, String optionFive) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optonOne;
+        OptionTwo = optionTwo;
+        OptionThree = optionThree;
+        OptionFour = optionFour;
+        OptionFive = optionFive;
+    }
+
+    public Question(int id, int imageResID, int correctAnswer, String questionText, String optionOne, String optionTwo, String optionThree) {
+        this.id = id;
+        this.imageResID = imageResID;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        OptionTwo = optionTwo;
+        OptionThree = optionThree;
+    }
+
+    public Question(int id, int correctAnswer, String questionText, String optionOne, String optionTwo, String optionThree, String optionFour) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        OptionTwo = optionTwo;
+        OptionThree = optionThree;
+        OptionFour = optionFour;
+    }
+
+    public Question(int id, int correctAnswer, String questionText, String optionOne, String optionTwo, String optionThree) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        OptionTwo = optionTwo;
+        OptionThree = optionThree;
+    }
+
+
+
+    public Question(int id, int correctAnswer, String questionText, String optionOne, String optionTwo) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        OptionTwo = optionTwo;
+    }
+
+    public Question(int id, int imageResID, int correctAnswer, String questionText, String optionOne, String optionTwo) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        this.imageResID = imageResID;
+        OptionTwo = optionTwo;
+    }
+
+    public Question(int id, int imageResID, int correctAnswer, String questionText, String optionOne, String optionTwo, String optionThree, String optionFour) {
+        this.id = id;
+        this.correctAnswer = correctAnswer;
+        this.questionText = questionText;
+        this.optionOne = optionOne;
+        this.imageResID = imageResID;
+        OptionTwo = optionTwo;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImageResID() {
+        return imageResID;
+    }
+
+    public void setImageResID(int imageResID) {
+        this.imageResID = imageResID;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getOptionOne() {
+        return optionOne;
+    }
+
+    public void setOptionOne(String optionOne) {
+        this.optionOne = optionOne;
+    }
+
+    public String getOptionTwo() {
+        return OptionTwo;
+    }
+
+    public void setOptionTwo(String optionTwo) {
+        OptionTwo = optionTwo;
+    }
+
+    public String getOptionThree() {
+        return OptionThree;
+    }
+
+    public void setOptionThree(String optionThree) {
+        OptionThree = optionThree;
+    }
+
+    public String getOptionFour() {
+        return OptionFour;
+    }
+
+    public void setOptionFour(String optionFour) {
+        OptionFour = optionFour;
+    }
+
+    public String getOptionFive() {
+        return OptionFive;
+    }
+
+    public void setOptionFive(String optionFive) {
+        OptionFive = optionFive;
+    }
+}
